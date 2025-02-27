54588680c2039bd4816e2f3120ef05395362c50a
nathan95oks <nathanaelalba75@gmail.com>
2025-02-27 09:15:33 -0400

    ingresar y devolver la cantidad con interfaz con pruebas

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
 import sumar from "./sumador";
+import totalizador from "./totalizador";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + totalizador(firstNumber ) + "</p>";
 });
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -5,6 +5,10 @@ it("deberia devolver la cantidad de productos",()=>{
    expect(totalizador(10)).toEqual(10);
  });
 
+ it("deberia devolver la cantidad de productos",()=>{
+    expect(totalizador(20)).toEqual(20);
+  });
+
 });
 
 
